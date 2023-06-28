332afc7f86ab5ba481e8f5d87c008e3b9fd84ec5
Diogo Felix <diogo2fex@gmail.com>
2023-06-28 13:01:25 -0300

    feat: first commit

---
+++ nuxt.config.js
@@ -0,0 +1,36 @@
+import { defineNuxtConfig } from '@nuxt/bridge'
+
+export default defineNuxtConfig( {
+  head: {
+    title: "Nuxt.js starter for CSB",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Official Nuxt.js starter for CodeSandBox"
+      }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+  /*
+   ** Nuxt.js modules
+   ** Doc: https://modules.nuxtjs.org
+   */
+  modules: [],
+  /*
+   ** Global CSS
+   ** Doc: https://nuxtjs.org/docs/2.x/configuration-glossary/configuration-css
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   ** Doc: https://nuxtjs.org/docs/2.x/directory-structure/plugins
+   */
+  plugins: [],
+  ssr: false,
+  target: 'static'
+});
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  Ö OE Ä AE Ü UE ä ae ö oe ü ue
+</template>
